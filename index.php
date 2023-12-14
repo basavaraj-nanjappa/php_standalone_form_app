6d2e0fcce78397da864a19baff1aef98b50223d2
basavaraj-nanjappa <127355458+basavaraj-nanjappa@users.noreply.github.com>
2023-12-14 20:11:29 +0530

    Update index.php
    minor change

--- index.php
+++ index.php
@@ -72,11 +72,11 @@
 </head>
 <body>
   <h1>Form Example</h1>
-
+ <div>  </div>
   <form method="POST" action="">
     <label for="name">Name:</label>
     <input type="text" id="name" name="name" required>
-
+ 
     <label for="age">Age:</label>
     <input type="number" id="age" name="age" required>
 
